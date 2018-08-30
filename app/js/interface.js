550c88b560ee0b0c1dd9f30b70338e55742bce35
Pavel-nineseven <pavel@nineseven.ru>
2018-08-30 14:59:21 +0300

    text+index(n-f)

--- app/js/interface.js
+++ app/js/interface.js
@@ -4,6 +4,96 @@ $(document).ready(function() {
 	// 	e.preventDefault();
 	// })
 
+
+	//TOP SUBMENU
+	$("body").on("click", ".js-top-links__link--submenu", function(e){
+		e.preventDefault();
+
+		$('.top-links__submenu').fadeToggle();
+	});
+	$(document).click(function (e){
+		var div = $(".top-links__item--sub");
+		if (!div.is(e.target)
+		    && div.has(e.target).length === 0) {
+			$('.top-links__submenu').fadeOut();
+		}
+	});
+
+
+	//MAIN SLIDER
+	if ($('.main-slider').length>0) {
+		$('.main-slider').slick({
+			// infinite: false,
+			slidesToShow: 1,
+			fade: true,
+			lazyLoad: 'progressive',
+			dots:true,
+			useTransform:true,
+			equalizeHeight: false,
+			"accessibility": false,
+			adaptiveHeight: true,
+			nextArrow: $('.main-slider__right'),
+  			prevArrow: $('.main-slider__left'),
+  			responsive: [
+			    {
+			      	breakpoint: 768,
+			      	settings: {
+				        infinite: false,
+						slidesToShow: 1,
+						fade: true,
+						lazyLoad: 'progressive',
+						useTransform:true,
+						"accessibility": false,
+						nextArrow: $('.main-slider__right'),
+			  			prevArrow: $('.main-slider__left'),
+			  			dots:true,
+			  			arrows:true,
+			    	}
+			    },
+		    ]
+		});
+	}
+
+
+	if ($('.main-news-slider').length>0) {
+        $('.main-news-slider').slick({
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: true,
+            lazyLoad: 'progressive',
+            useTransform:true,
+            "accessibility": false,
+            infinite:false,
+            dots:true,
+            appendArrows: '.slider-nav',
+			appendDots: '.slider-nav',
+            responsive: [
+                {
+                    breakpoint: 1150,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 700,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+            ]
+        });
+    }
+
+
+
+    //ACCORDEON
+	$("body").on("click", ".accordeon__link", function(e){
+		e.preventDefault();
+		$(this).parents('.accordeon__item').toggleClass('active');
+		$(this).next('.accordeon__info').slideToggle();
+	})
 });
 
 
@@ -21,16 +111,16 @@ $(window).resize(function () {
 
 
 // links pages
-// $('body').append(
-// 	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
-// 		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
-// 	<style> \
-// 		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
-// 		#pages a { text-decoration: none; } \
-// 		#pages li { margin: 5px 0; } \
-// 	</style> \
-// 	<ol id="pages"> \
-// 		<li><a href="about.html">About</a></li> \
-// 		<li><a href="index.html">Index</a></li> \
-// 	</ol> \
-// </div>');
+$('body').append(
+	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
+		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
+	<style> \
+		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
+		#pages a { text-decoration: none; } \
+		#pages li { margin: 5px 0; } \
+	</style> \
+	<ol id="pages"> \
+		<li><a href="text.html">Text</a></li> \
+		<li><a href="index.html">Index</a></li> \
+	</ol> \
+</div>');
